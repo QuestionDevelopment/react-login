30e46bf1b6b396cce022dfd61b3b16ff8087f1a5
mohith <mohithgm@gmail.com>
2017-04-28 21:15:30 +0530

    Fixed routes

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,12 @@ import Search from './search/search';
 class Routes extends React.Component {
   render() {
     return (
-      <Route path="/" component={App}>
-        <Route exact path="/" component={Home}/>
-        <Route path="/login"  component={Login} />
-        <Route path="/search/(:parms)" component={Search} />
+      <Route path="/">
+        <App>
+          <Route exact path="/" component={Home}/>
+          <Route path="/login"  component={Login} />
+          <Route path="/search/(:parms)" component={Search} />
+        </App>
       </Route>
     )
   }
