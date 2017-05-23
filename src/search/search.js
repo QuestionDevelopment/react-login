4287d2956095dc25f5cfc9be3124fd64209e3bec
Justin Campo <Justin@campo.cc>
2017-05-23 15:40:16 -0600

    Search Started

--- src/search/search.js
+++ src/search/search.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+	//
+
+//jsonbin.io/b/59237ee109fa8e50210e2306
+  componentDidMount() {
+		console.log('update');
+  }
+
   render() {
     return (
       <div className="Search">
+				<h1>Search Results</h1>
       </div>
     );
   }
